b12af48f72bf7d0ef255981d2fa5f00beab1b549
Jonathan Shine <jonathanhshine@gmail.com>
2021-08-10 11:32:12 +0200

    added functionality to the search in ComposerList to filter the list by query and added a button to reset to the original list

--- src/Components/ComposerList.jsx
+++ src/Components/ComposerList.jsx
@@ -3,25 +3,45 @@ import { useState, useContext } from 'react';
 import { DataContext } from '../App';
 
 const ComposerList = () => {
-    const { data } = useContext(DataContext);
+    const { data, setData } = useContext(DataContext);
+    const [query, setQuery] = useState('');
+    const [searchList, setSearchList] = useState({composers: []});
+    const [reset, setReset] = useState(true);
 
-    console.log(data);
- 
+    const handleChange = (e) => {
+        const query = e.target.value;
+        setQuery(query);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const tempArr = data.composers.filter(composer => {
+            return composer.complete_name.toLowerCase().includes(query);
+        });
+        console.log(tempArr);
+        setSearchList({composers: tempArr});
+        setQuery('');
+        setReset(false);
+    };
+    console.log(reset);
     return (
         <div>
             <h1>Composer List</h1>
 
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <div>
                 <label htmlFor="composerName">Search by Composer Name:</label>
-                <input onChange={(e) => handleChange(e)} type="search" name="composerName" id="composerName" />
+                <input onChange={(e) => handleChange(e)} type="text" name="composerName" id="composerName" value={query} />
             </div>
             <input type="submit" value="Search" />
+            <input onClick={() => setReset(!reset)} type="reset" value="Reset" />
         </form>
 
             <ul>
-                {data.composers.map(composer => {
-                    return <li key={composer.index}>{composer.complete_name}</li>
+                {reset ? data.composers.map(composer => {
+                    return <li key={composer.id}>{composer.complete_name}</li>
+                }) : searchList.composers.map(composer => {
+                    return <li key={composer.id}>{composer.complete_name}</li>
                 })}
             </ul>
         </div>
