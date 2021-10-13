cc10ccab8af22da94cd0bd4854c6da30fcf988a4
Jonathan Shine <jonathanhshine@gmail.com>
2021-10-13 15:37:41 +0200

    connected Login form to backend with call to API on submission

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { DataContext } from '../App';
+import { SignInUser } from '../helpers/apiCalls';
+
 
 export default function Login() {
+  const { user, setUser } = useContext( DataContext );
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const history = useHistory();
+
+  const submitHandler = async (info) => {
+    const res = await SignInUser(info);
+    if (!res.error) {
+      setUser(res);
+      history.push("/");
+      console.log("User successfully logged in");
+    } else {
+      console.log(res.error);
+    }
+  };
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit(submitHandler)}>
         <h3>Login</h3>
         <label>
           <span>Email</span>
-          <input type="text" placeholder="Enter Email" name="email" required/>
+          <input type="text" placeholder="Enter Email" name="email" { ...register("email", { required: true }) }/>
         </label>
+        {errors.email && <span>Email is required</span>}
 
         <label>
           <span>Password</span>
-          <input type="password" placeholder="Enter Password" name="psw" required/>
+          <input type="password" placeholder="Enter Password" name="psw" { ...register("password", { required: true }) }/>
         </label>
+        {errors.password && <span>Password is required</span>}
 
-        <button type="submit">submit</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   )
--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -12,3 +12,16 @@ export const SignUpUser = async (data) => {
         return error;
     };
 };
+
+export const SignInUser = async (data) => {
+    try {
+        const res = await( await fetch(`${serverURL}/users/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        })).json();
+        return res;
+    } catch (error) {
+        return error;
+    };
+};
